5bd34523b271bc77e536e7955512abcdfcba8a9f
Tia Theisen <tiatheisen@gmail.com>
2022-10-23 19:29:15 -0500

    update PUT and GET endpoints in gallery.router.js

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,24 +1,53 @@
 const express = require('express');
 const router = express.Router();
 const galleryItems = require('../modules/gallery.data');
+const pool = require('..modules/pool');
 
-// DO NOT MODIFY THIS FILE FOR BASE MODE
+
 
 // PUT Route
 router.put('/like/:id', (req, res) => {
     console.log(req.params);
     const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
-}); // END PUT Route
+
+    console.log('in PUT router put with id of:', galleryId);
+
+// UPDATE Like count
+    let sqlText = `
+    UPDATE "images"
+    SET "likes" = "likes" + 1
+    WHERE "id" = $1;`;
+
+    pool.query(sqlText, [itemId])
+        .then((dbRes) => {
+
+            res.sendStatus(200);
+        })
+        .catch(err => {
+            console.log('in router PUT error', err);
+            
+            res.sendStatus(500);
+        });
+});
+
+   
 
 // GET Route
 router.get('/', (req, res) => {
-    res.send(galleryItems);
-}); // END GET Route
+    console.log('in router GET');
+    const sqlText = `SELECT * FROM "images" ORDER BY id;`;
+
+    pool.query(sqlText)
+        .then((result) => {
+            console.log(`Got array back from the database`, result);
+
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log(`Error making database query`, error);
+
+            res.sendStatus(500);
+        })
+})
 
 module.exports = router;
